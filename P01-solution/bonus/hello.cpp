014d886735a477a977809317d802c463bfae4af4
Professor Rice <george.rice@uta.edu>
2020-01-17 17:55:15 -0600

    Fix repo merge

--- P01-solution/bonus/hello.cpp
+++
@@ -1,8 +0,0 @@
- #include <iostream>
- using namespace std;
- int main() {
-   string name;
-   cout << "What's your name? ";
-   cin >> name; // or getline(cin, name);
-   cout << "Hello, " << name << "!" << endl;
- }
--- P01-solution/bonus/hello_getline.cpp
+++
@@ -1,9 +0,0 @@
- #include <iostream>
- using namespace std;
- int main() {
-   string name;
-   cout << "What's your name? ";
-   getline(cin, name);
-   cout << "Hello, " << name << "!" << endl;
-   return 0;
- }
