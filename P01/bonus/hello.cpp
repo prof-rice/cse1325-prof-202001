4526dd7e93aa6bffeffb6221388c95f3dec0b926
Professor Rice <george.rice@uta.edu>
2020-01-17 18:03:35 -0600

    Update for lecture

--- P01/bonus/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::string s;
-    std::cout << "What's your name? ";
-    std::getline(std::cin, s);
-    std::cout << "Hello, " << s << std::endl;
-}
--- P01/full_credit/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Hello, Professor Rice!" << endl;
-}
